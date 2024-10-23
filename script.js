8866c5fa1cbf3f780fde8d4524cac0c56d99ef07
JVelezFD <jvelez117@gmail.com>
2024-10-23 13:22:56 -0500

    updated the score to 50 and removed the flowchart

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //.2 check score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       //finish game
       playing = false;
       document
